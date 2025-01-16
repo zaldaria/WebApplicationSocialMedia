5894f1ce1030cabb37d7ddf4bab789ecb1402f33
zaldaia <zaloznykhdaria@gmail.com>
2025-01-16 03:02:52 +0300

    #2

---
+++ WebApplicationSocialMedia/Areas/Identity/Pages/Account/EditProfile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationSocialMedia.Areas.Identity.Pages.Account
+{
+    public class EditProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
